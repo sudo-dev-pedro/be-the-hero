8c3df8a824407113da6841f4dae2eb1814b93d1b
Pedro Henrique <pedronoveseis@gmail.com>
2020-04-20 18:57:44 -0300

    Página de login, cadastro e de perfil - OK

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi/';
 
 import './style.css';
 
@@ -10,12 +12,34 @@ export default function Logon(){
     return (
 
         <div className="logon-container">
-
             <section className="form">
-                <img src={logo} alt="logo" />
+
+                <img 
+                    src={logo} 
+                    alt="Be The Hero" 
+                />
+
+                <form>
+                    <h1>Faça seu Login</h1>
+
+                    <input placeholder="Sua ID"/>
+                    
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="/registrar"> 
+                        <FiLogIn 
+                            size={16} 
+                            color="#E02041" 
+                        />
+                        Nao tenho cadastro
+                    </Link>
+                </form>
             </section>
 
-            <img src={heroesImage} alt="heroesImage" />
+            <img 
+                src={heroesImage} 
+                alt="heroesImage" 
+            />
 
         </div>
     );
---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FiPower } from 'react-icons/fi';
+import { FiTrash2 } from 'react-icons/fi';
+
+import './style.css';
+
+import logo from '../../assets/logo.svg';
+
+export default function Profile() {
+    
+    return(
+
+        <div className="profile-container">
+            <header>
+
+                <img 
+                    src={logo} 
+                    alt="Be The Hero" 
+                />
+
+                <span>Bem vinda, ONG</span>
+
+                <Link 
+                    className="button" 
+                    to="/caso/novo">Cadastar novo caso
+                </Link>
+                
+                <button type="button">
+                    <FiPower 
+                        size={18} 
+                        color="#E02041" 
+                    />
+                </button>
+
+            </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CASO: </strong>
+                    <p>Teste</p>
+
+                    <strong>DESCRICAO: </strong>
+                    <p>Teste</p>
+
+                    <strong>VALOR: </strong>
+                    <p>R$ 120,00</p>
+
+                    <button className="" type="button">
+                        <FiTrash2 
+                            size={20} 
+                            color="a8a8b3"
+                        />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>CASO: </strong>
+                    <p>Teste</p>
+
+                    <strong>DESCRICAO: </strong>
+                    <p>Teste</p>
+
+                    <strong>VALOR: </strong>
+                    <p>R$ 120,00</p>
+
+                    <button className="" type="button">
+                        <FiTrash2 
+                            size={20} 
+                            color="a8a8b3"
+                        />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>CASO: </strong>
+                    <p>Teste</p>
+
+                    <strong>DESCRICAO: </strong>
+                    <p>Teste</p>
+
+                    <strong>VALOR: </strong>
+                    <p>R$ 120,00</p>
+
+                    <button className="" type="button">
+                        <FiTrash2 
+                            size={20} 
+                            color="a8a8b3"
+                        />
+                    </button>
+                </li>
+                
+                <li>
+                    <strong>CASO: </strong>
+                    <p>Teste</p>
+
+                    <strong>DESCRICAO: </strong>
+                    <p>Teste</p>
+
+                    <strong>VALOR: </strong>
+                    <p>R$ 120,00</p>
+
+                    <button className="" type="button">
+                        <FiTrash2 
+                            size={20} 
+                            color="a8a8b3"
+                        />
+                    </button>
+                </li>
+
+            </ul>
+
+        </div>
+
+    );
+
+}
